0c70a83001d6be4d358500dec37a5b6cf4bf7275
eloiseboudon <boudon.eloise@gmail.com>
2025-07-11 08:45:36 +0200

    Document all route endpoints

--- backend/routes/main.py
+++ backend/routes/main.py
@@ -5,4 +5,13 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
+    """Simple health check endpoint.
+
+    ---
+    tags:
+      - Main
+    responses:
+      200:
+        description: API is reachable
+    """
     return {"message": "Hello World"}
